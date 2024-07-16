4766dc5bd5b6e3eb269f4d040a587ae35993bce0
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-07-16 15:37:55 +0700

    user login api

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,11 +3,27 @@ const app = express.Router();
 const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 const jwt = require('jsonwebtoken');
-
+const dotenv = require('dotenv')
 
 app.post('/signIn',async (req,res) =>{
     try {
-        res.send({ message: 'ok'})
+        const user = await prisma.user.findFirst({
+            select: {
+                id: true,
+                name: true
+            },
+            where: {
+                user: req.body.user,
+                pass: req.body.pass,
+                status: 'use'
+            }
+        })
+        if(user != null){
+            const secret = process.env.TOKEN_SECRET;
+            const token = jwt.sign( user, secret, {expiresIn: '30m'});
+            return res.send( { token: token  } );
+        }
+        res.status(401).send({ message: 'unauthorized' })
     } catch (e) {
         res.status(500).send({error: e.message})
     }
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const body = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors')
 
-app.use(body.json());
-app.use(body.urlencoded({ extended: true}));
-app.use(cors)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cors())
 const userController = require('./controllers/UserController')
 
 app.use('/user',userController);

233d046dbbb744fdea59d53efc0073944f5d22d6
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-07-19 11:17:15 +0700

    show image

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors())
+app.use('/uploads',express.static('uploads'))
 const userController = require('./controllers/UserController')
 const productController = require('./controllers/ProductController')
 

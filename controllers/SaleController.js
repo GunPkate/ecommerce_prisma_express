bbda98ade23148d95930c13c5c7c6c8a156f9657
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-07-23 11:23:57 +0700

    api save billdetail

--- controllers/SaleController.js
+++ controllers/SaleController.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv')
 
 app.post('/order',async (req,res) => {
     try {
-        const result = await prisma.billSale.create({
+        const resultBillSale = await prisma.billSale.create({
             data: {
                 customerName: req.body.customerName,
                 customerPhone: req.body.customerPhone,
@@ -16,8 +16,23 @@ app.post('/order',async (req,res) => {
                 payTime: req.body.payTime
             }
         })
+
+        for (let i = 0; i < req.body.cart.length; i++) {
+            let resultProduct = await prisma.product.findFirst({
+                where: { id: req.body.cart[i].id }
+            })
+            await prisma.billSaleDetail.create({
+                data: {
+                    billSaleId : resultBillSale.id,
+                    productId: resultProduct.id,
+                    cost: resultProduct.cost,
+                    price: resultProduct.price
+                }
+            })
+            // Prevent edit from Client 
+        }
+
         res.send({ message: 'success'})
-        console.log(req.body)
     } catch (e) {
         res.status(500).send({error: e.message})
     }
